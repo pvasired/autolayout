ba2c9546cccd40f73f367bf17075979e0c6d3810
pvasired <prafulv123@gmail.com>
2024-06-05 15:34:54 -0700

    checking for updates

---
+++ gds_script_testing.py
@@ -0,0 +1,93 @@
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
+import gdswriter
+import numpy as np
+from shapely.geometry import Polygon
+from shapely import buffer
+
+# Initialize the GDS design
+design = gdswriter.GDSDesign(size=(32000, 32000), units='um')
+
+# Define layers
+design.define_layer("Metal", 1, min_feature_size=1, min_spacing=1)
+design.define_layer("Oxide", 2, min_feature_size=1, min_spacing=1)
+
+# Parameters for circle creation
+circle_diameter1 = 5  # Diameter for the larger circles in um
+circle_diameter2 = 4   # Diameter for the smaller circles in um
+circle_radius1 = circle_diameter1 / 2
+circle_radius2 = circle_diameter2 / 2
+array_size = 2  # Size of the arrays (16x16)
+pitch = 30  # Pitch in um
+
+# Create a cell with a single larger circle for 'Metal' layer
+circle_cell_name1 = "Circle10um"
+design.add_cell(circle_cell_name1)
+design.add_circle_as_polygon(circle_cell_name1, center=(0, 0), radius=circle_radius1, layer_name="Metal", num_points=100)
+
+# Create an array of the larger circle cell on 'Metal' layer
+design.add_cell_array("TopCell", circle_cell_name1, copies_x=array_size, copies_y=array_size, spacing_x=pitch, spacing_y=pitch, origin=(0, 0),
+                      netIDs=np.array([[1, 2], [3, 4]]))
+
+# Create a cell with a single smaller circle for 'Via' layer
+circle_cell_name2 = "Circle8um"
+design.add_cell(circle_cell_name2)
+design.add_circle_as_polygon(circle_cell_name2, center=(0, 0), radius=circle_radius2, layer_name="Oxide", num_points=100)
+
+# Create an array of the smaller circle cell on 'Via' layer
+design.add_cell_array("TopCell", circle_cell_name2, copies_x=array_size, copies_y=array_size, spacing_x=pitch, spacing_y=pitch, origin=(0, 0),
+                      netIDs=np.array([[1, 2], [3, 4]]))
+
+pad_cell_name = "Pad"
+design.add_cell(pad_cell_name)
+design.add_rectangle(pad_cell_name, layer_name="Metal", center=(0, 0), width=120, height=1200)
+design.add_rectangle(pad_cell_name, layer_name="Oxide", center=(0, 0), width=100, height=1180)
+
+design.add_cell_array("TopCell", pad_cell_name, copies_y=1, copies_x=2, spacing_x=200, spacing_y=0, origin=(0, 1000),
+                      netIDs=np.array([[2, 4]]).T)
+design.add_cell_array("TopCell", pad_cell_name, copies_y=1, copies_x=2, spacing_x=200, spacing_y=0, origin=(0, -1000),
+                      netIDs=np.array([[1, 3]]).T)
+design.add_alignment_cross("TopCell", layer_name="Metal", center=(-350, -350), width=10, extent_x=100, extent_y=100)
+design.add_alignment_cross("TopCell", layer_name="Metal", center=(-350, 350), width=10, extent_x=100, extent_y=100)
+design.add_alignment_cross("TopCell", layer_name="Metal", center=(350, -350), width=10, extent_x=100, extent_y=100)
+design.add_alignment_cross("TopCell", layer_name="Metal", center=(350, 350), width=10, extent_x=100, extent_y=100)
+design.add_alignment_cross("TopCell", layer_name="Metal", center=(700, 700), width=10, extent_x=100, extent_y=100,
+                           netID=1)
+
+# Run design rule checks
+design.run_drc_checks()
+
+# Write to a GDS file
+design.write_gds("example_design.gds")
+
+top_cell_polygons = np.array(design.cells['TopCell']['polygons'], dtype=object)
+top_cell_netIDs = np.array(design.cells['TopCell']['netIDs'])
+
+routing_layer = 1
+trace_width = 2.0
+grid_size = 1.0
+
+routing_inds = np.where((top_cell_netIDs[:, 1] != 0) & (top_cell_netIDs[:, 0] == routing_layer))[0]
+uniqueIDs, inverse, counts = np.unique(top_cell_netIDs[routing_inds], axis=0, return_inverse=True, return_counts=True)
+routing_filter = np.where(counts > 1)[0]
+
+routing_groups = {}
+for i in range(len(routing_filter)):
+    routing_groups[i] = routing_inds[np.where(inverse == routing_filter[i])[0]]
+    shapely_polygons = [Polygon(polygon) for polygon in top_cell_polygons[routing_groups[i]]]
+    merged_polygons = gdswriter.cluster_intersecting_polygons(shapely_polygons)
+    path_reqs = [(polygon.centroid.x, polygon.centroid.y) for polygon in merged_polygons]
+
+    obstacles = np.setdiff1d(np.where(top_cell_netIDs[:, 0] == routing_layer)[0], routing_groups[i])
+    shapely_obstacles = [Polygon(polygon) for polygon in top_cell_polygons[obstacles]]
+    merged_obstacles = gdswriter.cluster_intersecting_polygons(shapely_obstacles)
+    buffered_obstacles = [polygon.buffer(trace_width) for polygon in merged_obstacles]
+
+    sx = path_reqs[0][0]
+    sy = path_reqs[0][1]
+    gx = path_reqs[1][0]
+    gy = path_reqs[1][1]
+
+    import pdb; pdb.set_trace()
+
+import pdb; pdb.set_trace()
