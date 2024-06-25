efcd8b8c6f2415323aee721dd268151b68a35540
pvasired <pvasired@stanford.edu>
2024-06-24 18:11:31 -0700

    fix

--- gds_script_read.py
+++ gds_script_read.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
 import gdswriter
@@ -10,17 +9,4 @@ design = gdswriter.GDSDesign(filename='Test_Structures.GDS')
 design.run_drc_checks()
 
 # Write to a GDS file
-=======
-import os, sys
-sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
-import gdswriter
-
-# Initialize the GDS design
-design = gdswriter.GDSDesign(filename='Test_Structures.GDS')
-
-# Run design rule checks
-design.run_drc_checks()
-
-# Write to a GDS file
->>>>>>> dcc204cbed7cce4a3e34a37e29f00ed11a775138
 design.write_gds("example_design-output.gds")
