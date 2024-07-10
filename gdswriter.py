b1f6923c82d02f94e77c65b05e123ef8ca0ebac4
pvasired <pvasired@stanford.edu>
2024-07-10 13:12:59 -0700

    updated for arbitrary rectangular arrays (still assumes N x N)

--- gdswriter.py
+++ gdswriter.py
@@ -694,15 +694,15 @@ class GDSDesign:
                 self.add_component(target_cell, target_cell_name, ref, netIDs[i][j] if netIDs is not None else cnt)
                 cnt += 1
 
-    def add_regular_array_escape(self, trace_cell_name, layer_name, pitch, array_size, trace_width, pad_diameter, escape_extent=50, routing_angle=45):
-        effective_pitch = pitch - pad_diameter
+    def add_regular_array_escape(self, trace_cell_name, layer_name, pitch_x, pitch_y, array_size, trace_width, pad_diameter, escape_extent=50, routing_angle=45, odd_extra_trace=False):
+        effective_pitch = pitch_x - pad_diameter
         self.add_cell(trace_cell_name)
 
         # Route outermost layer
 
         # Create the 2D grid using NumPy
-        x = np.linspace(-pitch*(array_size-1)/2, pitch*(array_size-1)/2, array_size)
-        y = np.linspace(-pitch*(array_size-1)/2, pitch*(array_size-1)/2, array_size)
+        x = np.linspace(-pitch_x*(array_size-1)/2, pitch_x*(array_size-1)/2, array_size)
+        y = np.linspace(-pitch_y*(array_size-1)/2, pitch_y*(array_size-1)/2, array_size)
         xx, yy = np.meshgrid(x, y, indexing='ij')
 
         # Stack the coordinates into a single 3D array
@@ -711,18 +711,6 @@ class GDSDesign:
         # Create the path points for the outermost layer
         for i in range(array_size-1):
             path_points = [(grid[i, 0, 0], grid[i, 0, 1]), (grid[i, 0, 0], grid[i, 0, 1] - escape_extent)]
-            self.add_path_as_polygon(trace_cell_name, path_points, trace_width, layer_name)
-
-        for i in range(array_size-1):
-            path_points = [(grid[-1, i, 0], grid[-1, i, 1]), (grid[-1, i, 0] + escape_extent, grid[-1, i, 1])]
-            self.add_path_as_polygon(trace_cell_name, path_points, trace_width, layer_name)
-
-        for i in range(array_size-1):
-            path_points = [(grid[-1-i, -1, 0], grid[-1, -1, 1]), (grid[-1-i, -1, 0], grid[-1, -1, 1] + escape_extent)]
-            self.add_path_as_polygon(trace_cell_name, path_points, trace_width, layer_name)
-
-        for i in range(array_size-1):
-            path_points = [(grid[0, -1-i, 0], grid[0, -1-i, 1]), (grid[0, -1-i, 0] - escape_extent, grid[0, -1-i, 1])]
             self.add_path_as_polygon(trace_cell_name, path_points, trace_width, layer_name)
 
         if (array_size-1) % 2 != 0:
@@ -845,6 +833,8 @@ class GDSDesign:
                         cnt += 1
 
                 iter_inds_R = np.flip(-np.arange(-special_column, 0, 2))
+                if odd_extra_trace:
+                    iter_inds_R = np.append(iter_inds_R, special_column+1)
                 assert (2*len(iter_inds_R)+1)*trace_width <= effective_pitch, "Not enough space for traces."
 
                 if len(iter_inds_R) == 1:
@@ -883,14 +873,6 @@ class GDSDesign:
                                                             grid[special_column+1][i][1]-grid[special_column+1][0][1]+escape_extent, post_rotation=90, post_reflection=True)
                     self.add_path_as_polygon(trace_cell_name, hinged_path, trace_width, layer_name)
                     cnt += 1
-    
-    def add_odd_center_trace(self, parent_cell_name, layer_name, center, pitch, array_size, trace_width, escape_extent=50):
-        center_electrode_path = []
-        center_electrode_path.append(center)
-        center_electrode_path.append((center[0]+pitch/2, center[1]-pitch))
-        center_electrode_path.append((center[0]+pitch*(array_size-1)/2-pitch/2, center[1]-pitch*(array_size-1)/2))
-        center_electrode_path.append((center[0]+pitch*(array_size-1)/2-pitch/2, center[1]-pitch*(array_size-1)/2-escape_extent))
-        self.add_path_as_polygon(parent_cell_name, center_electrode_path, trace_width, layer_name)
 
     def check_minimum_feature_size(self, cell_name, layer_name, min_size):
         # Assume `layer_number` is already determined from `layer_name`
