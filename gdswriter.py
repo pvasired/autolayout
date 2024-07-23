51c82fd07619b2b5a788225dd3496b78a6090a30
pvasired <pvasired@stanford.edu>
2024-07-23 13:16:18 -0700

    added 180 to 180 routing

--- gdswriter.py
+++ gdswriter.py
@@ -2625,8 +2625,10 @@ def cable_tie_ports(filename, cell_name, ports_, orientations, trace_width, laye
         D.write_gds(filename, cellname=top_cell_name)
     return wire_ports, wire_orientations
 
-def route_port_to_port(filename, cell_name, ports1_, orientations1, ports2_, orientations2, trace_width, layer_number,
+def route_port_to_port(filename, cell_name, ports1_, orientations1_, ports2_, orientations2_, trace_width, layer_number,
                        bbox1_=None, bbox2_=None, top_cell_name="TopCell"):
+    orientations1 = deepcopy(orientations1_)
+    orientations2 = deepcopy(orientations2_)
     assert len(ports1_) == len(ports2_)
     assert np.all(orientations1 == orientations1[0])
     assert np.all(orientations2 == orientations2[0])
@@ -2641,7 +2643,10 @@ def route_port_to_port(filename, cell_name, ports1_, orientations1, ports2_, ori
     D = pg.import_gds(filename, cellname=cell_name)
     # Seems to work: requires that ports2 is above ports1 in y
     cnt = 0
-    if orientations1[0] == 180 and orientations2[0] == 270:
+    if (orientations1[0] == 180 and orientations2[0] == 270) or (orientations1[0] == 270 and orientations2[0] == 180):
+        if orientations1[0] == 270:
+            ports1, ports2 = ports2, ports1
+            orientations1, orientations2 = orientations2, orientations1
         assert ports1[:, 1].max() < ports2[:, 1].min(), "Ports1 must be below Ports2 in y"
         ports1 = ports1[np.argsort(ports1[:, 1])]
         ports2 = ports2[np.argsort(ports2[:, 0])]
@@ -2718,7 +2723,10 @@ def route_port_to_port(filename, cell_name, ports1_, orientations1, ports2_, ori
                     cnt += 1
     
     # Kind of works: requires that ports2 is above ports1 in y
-    elif orientations1[0] == 90 and orientations2[0] == 270:
+    elif (orientations1[0] == 90 and orientations2[0] == 270) or (orientations1[0] == 270 and orientations2[0] == 90):
+        if orientations1[0] == 270:
+            ports1, ports2 = ports2, ports1
+            orientations1, orientations2 = orientations2, orientations1
         assert ports1[:, 1].max() < ports2[:, 1].min(), "Ports1 must be below Ports2 in y"
         ports1 = ports1[np.argsort(ports1[:, 0])]
         ports2 = ports2[np.argsort(ports2[:, 0])]
@@ -2782,7 +2790,10 @@ def route_port_to_port(filename, cell_name, ports1_, orientations1, ports2_, ori
                 additional_y += 2*trace_width
 
     # Seems to work: requires that ports2 is above ports1 in y
-    elif orientations1[0] == 0 and orientations2[0] == 270:
+    elif (orientations1[0] == 0 and orientations2[0] == 270) or (orientations1[0] == 270 and orientations2[0] == 0):
+        if orientations1[0] == 270:
+            ports1, ports2 = ports2, ports1
+            orientations1, orientations2 = orientations2, orientations1
         assert ports1[:, 1].max() < ports2[:, 1].min(), "Ports1 must be below Ports2 in y"
         ports1 = ports1[np.flip(np.argsort(ports1[:, 1]))]
         ports2 = ports2[np.argsort(ports2[:, 0])]
@@ -2858,7 +2869,10 @@ def route_port_to_port(filename, cell_name, ports1_, orientations1, ports2_, ori
                     cnt += 1
     
     # Kind of works: requires that ports1 is above ports2 in y
-    elif orientations1[0] == 180 and orientations2[0] == 90:
+    elif (orientations1[0] == 180 and orientations2[0] == 90) or (orientations1[0] == 90 and orientations2[0] == 180):
+        if orientations1[0] == 90:
+            ports1, ports2 = ports2, ports1
+            orientations1, orientations2 = orientations2, orientations1
         assert ports1[:, 1].min() > ports2[:, 1].max(), "Ports1 must be above Ports2 in y"
         ports1 = ports1[np.argsort(ports1[:, 1])]
         ports2 = ports2[np.flip(np.argsort(ports2[:, 0]))]
@@ -2934,7 +2948,10 @@ def route_port_to_port(filename, cell_name, ports1_, orientations1, ports2_, ori
                     cnt += 1      
     
     # Kind of works: requires that ports1 is above ports2 in y
-    elif orientations1[0] == 0 and orientations2[0] == 90:
+    elif (orientations1[0] == 0 and orientations2[0] == 90) or (orientations1[0] == 90 and orientations2[0] == 0):
+        if orientations1[0] == 90:
+            ports1, ports2 = ports2, ports1
+            orientations1, orientations2 = orientations2, orientations1
         assert ports1[:, 1].min() > ports2[:, 1].max(), "Ports1 must be above Ports2 in y"
         ports1 = ports1[np.argsort(ports1[:, 1])]
         ports2 = ports2[np.argsort(ports2[:, 0])]
@@ -3009,7 +3026,10 @@ def route_port_to_port(filename, cell_name, ports1_, orientations1, ports2_, ori
                     cnt += 1
 
     # Kind of works: requires that ports1 is left of ports2 in x
-    elif orientations1[0] == 0 and orientations2[0] == 180:
+    elif (orientations1[0] == 0 and orientations2[0] == 180) or (orientations1[0] == 180 and orientations2[0] == 0):
+        if orientations1[0] == 180:
+            ports1, ports2 = ports2, ports1
+            orientations1, orientations2 = orientations2, orientations1
         assert ports1[:, 0].max() < ports2[:, 0].min(), "Ports1 must be left of Ports2 in x"
         ports1 = ports1[np.flip(np.argsort(ports1[:, 1]))]
         ports2 = ports2[np.flip(np.argsort(ports2[:, 1]))]
@@ -3118,6 +3138,32 @@ def route_port_to_port(filename, cell_name, ports1_, orientations1, ports2_, ori
         for i, port in enumerate(ports1):
             port1 = D.add_port(name=f"Electrode {cnt}", midpoint=(port[0], port[1]), width=trace_width, orientation=orientations1[0])
             port2 = D.add_port(name=f"Pad {cnt}", midpoint=(ports2[i][0], ports2[i][1]+2*i*trace_width), width=trace_width, orientation=orientations2[0])
+
+            D.add_ref(pr.route_smooth(port1, port2, width=trace_width, layer=layer_number, radius=trace_width))
+
+            P = Path([ports2[i], port2.midpoint])
+            path = P.extrude(trace_width, layer=layer_number)
+            D.add_ref(path)
+            cnt += 1
+    
+    elif orientations1[0] == orientations2[0] == 180:
+        assert bbox1 is not None and bbox2 is not None, "Bounding boxes must be provided for this orientation"
+        if ports1[:, 0].min() < ports2[:, 0].min():
+            ports1, ports2 = ports2, ports1
+            bbox1, bbox2 = bbox2, bbox1
+
+        assert ports1[:, 1].max() < bbox2[0][1] - 3*trace_width/2 or ports1[:, 1].min() > bbox2[1][1] + 3*trace_width/2, "No space for routing"
+
+        if ports1[:, 1].max() < ports2[:, 1].min():
+            ports1 = ports1[np.flip(np.argsort(ports1[:, 1]))]
+            ports2 = ports2[np.argsort(ports2[:, 1])]
+        else:
+            ports1 = ports1[np.argsort(ports1[:, 1])]
+            ports2 = ports2[np.flip(np.argsort(ports2[:, 1]))]
+
+        for i, port in enumerate(ports1):
+            port1 = D.add_port(name=f"Electrode {cnt}", midpoint=(port[0], port[1]), width=trace_width, orientation=orientations1[0])
+            port2 = D.add_port(name=f"Pad {cnt}", midpoint=(ports2[i][0]-2*i*trace_width, ports2[i][1]), width=trace_width, orientation=orientations2[0])
 
             D.add_ref(pr.route_smooth(port1, port2, width=trace_width, layer=layer_number, radius=trace_width))
 
