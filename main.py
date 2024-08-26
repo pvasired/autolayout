d5b08f9cc392fc135bca64c9e6cf6f1f5daa08fe
pvasired <pvasired@stanford.edu>
2024-08-26 12:36:23 -0700

    turned back off animations

--- main.py
+++ main.py
@@ -2210,7 +2210,7 @@ class MyApp(QWidget):
 
                     self.gds_design.route_ports_a_star(self.cellComboBox.currentText(), ports1, orientations1,
                                                 ports2, orientations2, trace_width1, trace_space1, layer_name,
-                                                show_animation=True, obstacles=obstacles)
+                                                show_animation=False, obstacles=obstacles)
 
                     # Remove the routed ports from the corresponding escapeDicts
                     for escapeDict in self.escapeDicts[self.cellComboBox.currentText()]:
@@ -2282,7 +2282,7 @@ class MyApp(QWidget):
                 self.addSnapshot()  # Store snapshot before adding new design
                 try:
                     self.gds_design.route_trace_a_star(self.cellComboBox.currentText(), start, end, trace_width, trace_space, layer_name, 
-                                                    obstacles=obstacles, show_animation=True)
+                                                    obstacles=obstacles, show_animation=False)
                     self.writeToGDS()
                     self.updateAvailableSpace()
                     self.update_plot_data()
